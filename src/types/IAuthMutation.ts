c7266c6bd65896f742659fb694c1a9c61fd900e2
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-01-20 12:03:08 +0100

    feat(login): Completes login mutation
    - Check that a user with the email exists
    - Check if their password matches
    - Return a token and user profile if it matches else throw an error
    
    [Completes #9]

--- src/types/IAuthMutation.ts
+++ src/types/IAuthMutation.ts
@@ -1,7 +1,13 @@
-import { IContext } from "./IContext";
+import { IContext } from './IContext';
 
-export type ResolverFunction = (parent: any, args: any, context: IContext, info?: any) => any;
+export type ResolverFunction = (
+  parent: any,
+  args: any,
+  context: IContext,
+  info?: any,
+) => any;
 
 export abstract class IAuthMutation {
   static signup: ResolverFunction;
+  static login: ResolverFunction;
 }
