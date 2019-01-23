bfc432cc557b0150155d7ed835be7cfd30abca15
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-01-23 22:37:38 +0100

    feat(resolvers): Implement resolvers for type relation fields
    - Add resolver function to the relation fields on the types
    
    [Completes #20]

--- src/resolvers/Book.ts
+++ src/resolvers/Book.ts
@@ -1,15 +1,19 @@
 import { BookResolvers } from '../types/graphqlgen';
+import { Publisher, User, Rating } from '../types/types';
 
 export const Book: BookResolvers.Type = {
   ...BookResolvers.defaultResolvers,
 
-  publishers: (parent, args, ctx) => {
-    throw new Error('Resolver not implemented');
+  publishers: async ({ id }, args, { prisma }): Promise<Publisher[]> => {
+    const publishers: Publisher[] = await prisma.book({ id }).publishers();
+    return publishers;
   },
-  authors: (parent, args, ctx) => {
-    throw new Error('Resolver not implemented');
+  authors: async ({ id }, args, { prisma }): Promise<User[]> => {
+    const authors: User[] = await prisma.book({ id }).authors();
+    return authors;
   },
-  ratings: (parent, args, ctx) => {
-    throw new Error('Resolver not implemented');
+  ratings: async ({ id }, args, { prisma }): Promise<Rating[]> => {
+    const ratings: Rating[] = prisma.book({ id }).ratings();
+    return ratings;
   },
 };
